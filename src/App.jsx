b402ad5bbb29fccdd13a8abd4375fb85dd044eaf
Rebekah <bekahramirez08@gmail.com>
2025-10-27 15:21:46 -0700

    Added SearchBar Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-
+import SearchBar from './Components/header/searchbar'
 import './App.css'
 
 function App() {
+  return (
+    <SearchBar/>
+  )
 }
 
 export default App
--- src/Components/header/searchbar.jsx
+++ src/Components/header/searchbar.jsx
@@ -1,3 +1,28 @@
 import { fetchSearchResults } from "../../features/searchSlice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+
+
+ const SearchBar = () => {
+    const [search, setSearch] = useState('');
+    const dispatch = useDispatch();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(fetchSearchResults(search));
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={search} 
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Search reddit"
+        />
+        <button type="submit">Search</button>
+      </form>
+    )
+}
+
+export default SearchBar;
