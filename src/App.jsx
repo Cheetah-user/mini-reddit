d47143ae59e8d950270c4a4d4fdb00979b9386e5
Rebekah <bekahramirez08@gmail.com>
2025-10-28 17:14:21 -0700

    Added Posts logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import SearchBar from './Components/header/searchbar'
 import Comments from './components/comments/comments'
+import Posts from './Components/posts/posts'
 import './App.css'
 
 function App() {
   return (
    <>
     <SearchBar/>
-    <Comments/>  
+    <Comments/> 
+    <Posts/> 
    </>
   )
 }
